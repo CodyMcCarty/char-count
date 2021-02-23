0e189d0589427b5e594b05f0eec869a552369c79
Cody McCarty <codyjmccarty@gmail.com>
2021-02-22 19:23:42 -0600

    Py test setup

--- python/char_count_spec.py
+++ python/char_count_spec.py
@@ -1,27 +1,65 @@
 # Can you translate this driver code to unit tests?
 
 from char_count import char_count
+import unittest
+class CharCountTestCase(unittest.TestCase):
+  "Test for char_count"
 
-print(char_count("aaabbc") == {
-  "a": 3,
-  "b": 2,
-  "c": 1
-})
+  def test_returns_an_object(self):
+    """When you call char_count(), you should get an object back"""
+    self.assertEqual(type(char_count('str')), dict)
 
-print(char_count("an apple a day will keep the doctor away") == {
-  "a":6,
-  "e":4,
-  "l":3,
-  "p":3,
-  "w":2,
-  "d":2,
-  "o":2,
-  "t":2,
-  "y":2,
-  "k":1,
-  "h":1,
-  "i":1,
-  "c":1,
-  "n":1,
-  "r":1
-})
+  def test_checks_aaabbc(self):
+    """When you call char_count('aaabbc'), you should get the correct object back"""
+    self.assertEqual(char_count('aaabbc'), {
+      "a": 3,
+      "b": 2,
+      "c": 1
+      })
+
+  def test_checks_an_apple_a_day_will_keep_the_doctor_away(self):
+    """When you call char_count('an apple a day will keep the doctor away'), you should get the correct object back"""
+    self.assertEqual(char_count('an apple a day will keep the doctor away'), {
+      "a": 6,
+      "e": 4,
+      "p": 3,
+      "l": 3,
+      "y": 2,
+      "w": 2,
+      "t": 2,
+      "d": 2,
+      "h": 1,
+      "n": 1,
+      "c": 1,
+      "o": 2,
+      "i": 1,
+      "k": 1,
+      "r": 1
+    })
+
+if __name__ == '__main__':
+  unittest.main()
+
+# print(char_count("aaabbc") == {
+#   "a": 3,
+#   "b": 2,
+#   "c": 1
+# })
+
+# print(char_count("an apple a day will keep the doctor away") == {
+#   "a":6,
+#   "e":4,
+#   "l":3,
+#   "p":3,
+#   "w":2,
+#   "d":2,
+#   "o":2,
+#   "t":2,
+#   "y":2,
+#   "k":1,
+#   "h":1,
+#   "i":1,
+#   "c":1,
+#   "n":1,
+#   "r":1
+# })
