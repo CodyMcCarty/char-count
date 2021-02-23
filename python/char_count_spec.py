bc05594107b73982af7cb9ab5db8fcaa410ca233
Cody McCarty <codyjmccarty@gmail.com>
2021-02-22 20:11:26 -0600

    PY and JS complete

--- python/char_count_spec.py
+++ python/char_count_spec.py
@@ -1,7 +1,7 @@
-# Can you translate this driver code to unit tests?
-
-from char_count import char_count
 import unittest
+
+# begin test for char_count
+from char_count import char_count
 class CharCountTestCase(unittest.TestCase):
   "Test for char_count"
 
@@ -40,26 +40,4 @@ class CharCountTestCase(unittest.TestCase):
 if __name__ == '__main__':
   unittest.main()
 
-# print(char_count("aaabbc") == {
-#   "a": 3,
-#   "b": 2,
-#   "c": 1
-# })
-
-# print(char_count("an apple a day will keep the doctor away") == {
-#   "a":6,
-#   "e":4,
-#   "l":3,
-#   "p":3,
-#   "w":2,
-#   "d":2,
-#   "o":2,
-#   "t":2,
-#   "y":2,
-#   "k":1,
-#   "h":1,
-#   "i":1,
-#   "c":1,
-#   "n":1,
-#   "r":1
-# })
+# end test for char_count
